05cba4ae8ad6cf307bcdd1d3b08326f2966491d7
ViniciusJMR <viniciusdejesus605@gmail.com>
2022-01-12 22:49:31 -0300

    Criado o projeto

---
+++ api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/ApiViniciusApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.apivinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViniciusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiViniciusApplication.class, args);
+	}
+
+}
---
+++ api-vinicius/src/test/java/com/algaworks/algalog/apivinicius/ApiViniciusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.apivinicius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiViniciusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
