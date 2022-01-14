b759e8e8a550c81e4ce0dd4f02d0cec1f2c46387
ViniciusJMR <viniciusdejesus605@gmail.com>
2022-01-14 19:52:58 -0300

    Implementado CRUD do Cliente

--- api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/api/controller/ClienteController.java
+++ api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/api/controller/ClienteController.java
@@ -3,22 +3,64 @@ package com.algaworks.algalog.apivinicius.api.controller;
 import com.algaworks.algalog.apivinicius.domain.model.Cliente;
 import com.algaworks.algalog.apivinicius.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
-        return clienteRepository.findByNameContaining("da");
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+        return clienteRepository.findById(clienteId)
+                //.map(cliente -> ResponseEntity.ok(cliente))
+                .map(ResponseEntity::ok) //Method Reference
+                .orElse(ResponseEntity.notFound().build());
+
+        /*
+        Forma maior de se fazer o uqe está acima
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+        if (cliente.isPresent())
+            return ResponseEntity.ok(cliente.get());
+
+        return ResponseEntity.notFound().build();
+         */
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente criar(@Valid @RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente){
+        if (!clienteRepository.existsById(clienteId))
+            return ResponseEntity.notFound().build();
+        cliente.setId(clienteId);
+        return ResponseEntity.ok(clienteRepository.save(cliente));
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteId){
+        if (!clienteRepository.existsById(clienteId))
+            return ResponseEntity.notFound().build();
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/domain/model/Cliente.java
+++ api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -17,9 +20,18 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     @Column(name = "nome")
     private String name;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "telefone")
     private String phone;
 }
