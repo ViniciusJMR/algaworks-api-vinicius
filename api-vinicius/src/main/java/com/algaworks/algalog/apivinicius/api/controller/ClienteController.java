08ae1e672d0784ddf2c5602092ba7e320c70695c
ViniciusJMR <viniciusdejesus605@gmail.com>
2022-01-14 00:35:18 -0300

    Aula 2: Configurado o Flyway e Configurado o JPA

--- api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/api/controller/ClienteController.java
+++ api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/api/controller/ClienteController.java
@@ -4,20 +4,20 @@ import com.algaworks.algalog.apivinicius.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        var c1 = new Cliente();
-        c1.setId(1L);
-        c1.setName("João");
-        c1.setEmail("joao@gmail.com");
-        c1.setPhone("aaaaaaaaa");
-
-        return Arrays.asList(c1, c1);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/domain/model/Cliente.java
+++ api-vinicius/src/main/java/com/algaworks/algalog/apivinicius/domain/model/Cliente.java
@@ -1,14 +1,25 @@
 package com.algaworks.algalog.apivinicius.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome")
     private String name;
     private String email;
+    @Column(name = "telefone")
     private String phone;
 }
